8f56a2d618b25f48025d74eb00de94a0e8f75410
Gilles Coomans <gilles.coomans@gmail.com>
2014-03-06 14:43:32 +0100

    rewrite store as classe + homogeneise createDefault + add contextualised rootPath management

--- index.js
+++ index.js
@@ -16,122 +16,131 @@ define(["require", "deepjs/deep", "./init"],function (require, deep)
 		return swig.compile(string, options);
 	};
 
+	if(deep.isNode)
+	{
+		var fs = require("fs");
+		deep.client.Swig = deep.compose.Classes(deep.Store, function(protocol, basePath, options){
+			console.log("swig constructor : ", protocol, basePath, options)
+			this.basePath = basePath || this.basePath || "";
+			if(options)
+				deep.utils.up(options, this);
+			this.watched = this.watched || {};
+		}, {
+			responseParser : function(datas, path){
+				if(datas instanceof Buffer)
+					datas = datas.toString("utf8");
+				var res = swig.compile(datas);
+				return res;
+			},
+			get:function(path, options)
+			{
+				options = options || {};
+				path = (deep.context.rootPath || deep.globals.rootPath || "")+this.basePath+path;
+				var cacheName = "swig::"+path;
+				if(options.cache !== false && deep.mediaCache.cache[cacheName])
+					return deep.mediaCache.cache[cacheName];
+				var def = deep.Deferred(),
+					self = this;
+				if(!this.watched[path])
+					this.watched[path] = fs.watch(path, function (event, filename) {
+						switch(event)
+						{
+							case 'change' :
+								//console.log("deep swig : changes detected : reload")
+								fs.readFile(path, function(err, datas){
+									var d = null;
+									if(err)
+										d = deep.when(deep.errors.Watch("Error while reloading file : "+path));
+									else
+										d = deep.when(self.responseParser(datas, path));
+									deep.mediaCache.manage(d, cacheName);
+								});
+								break;
+							case 'rename' :
+								deep.mediaCache.remove(cacheName);
+								break;
+						}
+					});
+				fs.readFile(path, function(err, datas)
+				{
+					if(err)
+						return def.reject(err);
+					def.resolve(self.responseParser(datas, path));
+				});
+				var d = def.promise();
+				if(options.cache !== false)
+					deep.mediaCache.manage(d, cacheName);
+				return d;
+			}
+		});
+	}
+	else
+	{
+		deep.client.Swig = deep.compose.Classes(deep.Store, function(protocol, basePath, options){
+			this.basePath = basePath || this.basePath || "";
+			if(options)
+				deep.utils.up(options, this);
+			this.watched = this.watched || {};
+		}, {
+			writeHeaders : function (req, headers)
+			{
+				for(var i in deep.globalHeaders)
+					req.setRequestHeader(i, deep.globalHeaders[i]);
+				for(i in this.headers)
+					req.setRequestHeader(i, this.headers[i]);
+				for(i in headers)
+					req.setRequestHeader(i, headers[i]);
+			},
+			get : function (id, options) {
+				//console.log("swig store : ", id, options)
+				options = options || {};
+				path = (deep.context.rootPath || deep.globals.rootPath || "")+this.basePath+path;
+				var cacheName = "swig::"+id;
+				if(options.cache !== false && deep.mediaCache.cache[cacheName])
+					return deep.mediaCache.cache[cacheName];
+				var self = this;
+				var def = deep.Deferred();
+				var promise = $.ajax({
+					beforeSend :function(req) {
+						self.writeHeaders(req, {
+							"Accept" : "text/html; charset=utf-8"
+						});
+					},
+					url:id,
+					method:"GET"
+				})
+				.done(function(data, msg, jqXHR){
+					def.resolve(data);
+				})
+				.fail(function(){
+					def.reject(deep.errors.Protocole("deep.client.Swig failed : "+id+" - \n\n"+JSON.stringify(arguments)));
+				});
+				//console.log("ajax promise : ", promise);
+				var d = def.promise()
+				.done(function (data) {
+					var resi = swig.compile(data, { filename:deep.utils.stripFirstSlash(id) });
+					//console.log("swig store : resi ", resi);
+					return resi;
+				});
+				if(options.cache !== false)
+					deep.mediaCache.manage(d, cacheName);
+				return d;
+			}
+		});
+	}
 	//__________________________________________________
-	deep.client.Swig = new deep.Store();
 	deep.extensions.push({
 		store:deep.client.Swig,
 		extensions : [
 			/(\.(swig)(\?.*)?)$/gi
 		]
 	});
-
-	if(deep.isNode)
-	{
-		var fs = require("fs");
-		//__________________________________________________	
-		deep.client.Swig.watched = {};
-		deep.client.Swig.protocol = "swig";
-		deep.client.Swig.responseParser = function(datas, path){
-			if(datas instanceof Buffer)
-				datas = datas.toString("utf8");
-
-			var res = swig.compile(datas);
-			//console.log("swig loaded : res : ", res);
-			return res;
-		};
-		deep.client.Swig.get = function (path, options) {
-			options = options || {};
-			var cacheName = "swig::"+path;
-			if(options.cache !== false && deep.mediaCache.cache[cacheName])
-				return deep.mediaCache.cache[cacheName];
-			var def = deep.Deferred(),
-				self = this;
-				
-			if(!this.watched[path])
-				this.watched[path] = fs.watch(path, function (event, filename) {
-					switch(event)
-					{
-						case 'change' :
-							//console.log("deep swig : changes detected : reload")
-							fs.readFile(path, function(err, datas){
-								var d = null;
-								if(err)
-									d = deep.when(deep.errors.Watch("Error while reloading file : "+path));
-								else
-									d = deep.when(self.responseParser(datas, path));
-								deep.mediaCache.manage(d, cacheName);
-							});
-							break;
-						case 'rename' :
-							deep.mediaCache.remove(cacheName);
-							break;
-					}
-				});
-
-			fs.readFile(path, function(err, datas)
-			{
-				if(err)
-					return def.reject(err);
-				def.resolve(self.responseParser(datas, path));
-			});
-			var d = def.promise();
-			if(options.cache !== false)
-				deep.mediaCache.manage(d, cacheName);
-			return d;
-		};
-	}
-	else
-	{
-		var writeHeaders = function (req, headers)
-		{
-			for(var i in deep.globalHeaders)
-				req.setRequestHeader(i, deep.globalHeaders[i]);
-			for(i in this.headers)
-				req.setRequestHeader(i, this.headers[i]);
-			for(i in headers)
-				req.setRequestHeader(i, headers[i]);
-		};
-		deep.client.Swig.get = function (id, options) {
-			//console.log("swig store : ", id, options)
-			options = options || {};
-			var cacheName = "swig::"+id;
-			if(options.cache !== false && deep.mediaCache.cache[cacheName])
-				return deep.mediaCache.cache[cacheName];
-			var self = this;
-			var def = deep.Deferred();
-			var promise = $.ajax({
-				beforeSend :function(req) {
-					writeHeaders(req, {
-						"Accept" : "text/html; charset=utf-8"
-					});
-				},
-				url:id,
-				method:"GET"
-			})
-			.done(function(data, msg, jqXHR){
-				def.resolve(data);
-			})
-			.fail(function(){
-				def.reject(deep.errors.Protocole("deep.client.Swig failed : "+id+" - \n\n"+JSON.stringify(arguments)));
-			});
-			//console.log("ajax promise : ", promise);
-			var d = def.promise()
-			.done(function (data) {
-				var resi = swig.compile(data, { filename:deep.utils.stripFirstSlash(id) });
-				//console.log("swig store : resi ", resi);
-				return resi;
-			});
-			if(options.cache !== false)
-				deep.mediaCache.manage(d, cacheName);
-			return d;
-		};
-	}
-	var init = require("./init");
-	deep.client.Swig.createDefault = function(options){
-		init(options);
-		deep.protocols.swig = this;
-		return this;
-	}
+	deep.client.Swig.init = require("./init");
+	deep.client.Swig.create = deep.client.Swig.createDefault = function(protocol, basePath, options){
+		if(typeof protocol === 'undefined')
+			protocol = "swig";
+		return new deep.client.Swig(protocol, basePath, options);
+	};
 	return deep.client.Swig;
 });
 
